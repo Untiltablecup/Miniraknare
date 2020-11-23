b2b4d9d45919e8626fe8475733e22439b53f38dc
Untiltablecup <pelle.eriksson@elev.ga.ntig.se>
2020-11-23 13:02:25 +0100

    Miniräknare början

---
+++ Miniräknare.py
@@ -0,0 +1,12 @@
+while True:
+    räkna_med = int(input("Välkommen till miniräknaren.\n[1] Räkna med +\n[2] Räkna med -\n[3] Räkna med *\n[4] Räkna med /\n[5] Avsluta\n>"))
+    if räkna_med == "1":
+        nummer_1 = input(float("Skriv ett tal\n>"))
+        nummer_2 = 
+    elif räkna_med == "2":
+
+    elif räkna_med == "3":
+
+    elif räkna_med == "4":
+
+    else:
